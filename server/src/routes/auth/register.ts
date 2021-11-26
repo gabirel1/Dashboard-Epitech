f80d6cd45b0bc9c240b41b9c177040add98165b3
gabirel1 <gabk2000@gmail.com>
2021-11-27 00:07:32 +0100

    [main]: fixing register

--- server/src/routes/auth/register.ts
+++ server/src/routes/auth/register.ts
@@ -1,17 +1,36 @@
 import express from "express"
-import { registerPlainUser } from "./registerActions";
+import { UserInformations } from "../../database/interfaces";
+import { registerBasicUser } from "./registerActions";
 
 class Register {
     async post(req: express.Request, res: express.Response) {
-        const type = req.params.type;
-        console.log(req.params);
+        const { mail, password } = req.body;
 
-        switch (type) {
-            case "plain_user":
-                return registerPlainUser(req, res);
-            case "":
-                return res.send(405).json({ error: true, message: "type not found" });
+        const infos: UserInformations = {
+            mail: mail,
+            google_mail: undefined,
+            google_token: undefined,
+            facebook_mail: undefined,
+            facebook_token: undefined,
+            apple_mail: undefined,
+            apple_token: undefined,
+            office_mail: undefined,
+            office_token: undefined,
+            username: undefined,
+            password: password,
+        };
+        let result = await registerBasicUser(infos);
+
+        if (result.error === true) {
+            return res.status(500).json({
+                error: true,
+                message: result.message,
+            });
         }
+        if (result.error === false && result.message === "user already exists") {
+            return res.status(403).json({ error: true, message: result.message });
+        }
+        return res.status(200).json({ error: false, message: result.message, result: result.result });
     }
 }
 
--- server/src/routes/auth/registerActions.ts
+++ server/src/routes/auth/registerActions.ts
@@ -1,29 +1,65 @@
 import express from 'express';
-import { addGoogleUser, addUser } from '../../database/actions';
+import { getUsers, addUsers } from '../../database/databaseActions';
+// import { addGoogleUser, addUser } from '../../database/actions';
+import { UserInformations } from '../../database/interfaces';
 
-export const registerPlainUser = (req: express.Request, res: express.Response) => {
-    const { mail, username, password } = req.body;
-        console.log(req.body);
+// export const registerPlainUser = (req: express.Request, res: express.Response) => {
+//     const { mail, username, password } = req.body;
+//         console.log(req.body);
         
-        addUser(mail, username, password, (err: any, result: any) => {
+//         addUser(mail, username, password, (err: any, result: any) => {
+//             if (err) {
+//                 console.log(err);
+//                 return res.status(409).json({ error: true, message: "username or email already exists" });
+//             } else {
+//                 return res.status(200).json({ error: false, message: "user created", result: result });
+//             }
+//     });
+// }
+
+// export const registerGoogleUser = (req: express.Request, res: express.Response) => {
+//     const { mail, username } = req.body;
+
+//     addGoogleUser(username, mail, (err: any, result: any) => {
+//         if (err) {
+//             console.log(err);
+//             return res.status(409).json({ error: true, message: "You might already have registered with this account" });
+//         } else {
+//             return res.status(200).json({ error: false, message: "user created", result: result });
+//         }
+//     });
+// }
+
+export const registerBasicUser = async (infos: UserInformations): Promise<{ error: boolean, message: string, result?: any }> => {
+    return new Promise(async (resolve, reject) => {
+        await getUsers(infos, async (err: any, result: any) => {
             if (err) {
-                console.log(err);
-                return res.status(409).json({ error: true, message: "username or email already exists" });
+                console.debug('[handleOAuthUsersAction] | error[getUsers] = ', err);
+                await addUsers(infos, (err: any, result: any) => {
+                    if (err) {
+                        console.debug('[handleOAuthUsersAction] | error[addUsers] = ', err);
+                        return resolve({ error: true, message: "internal error" });
+                    } else {
+                        console.debug('[handleOAuthUsersAction] | result[addUsers] = ', result);
+                        return resolve({ error: false, message: "user created", result: result });
+                    }
+                });
             } else {
-                return res.status(200).json({ error: false, message: "user created", result: result });
+                console.debug('[handleOAuthUsersAction] | result[getUsers] = ', result);
+                if (result.length === 0) {
+                    await addUsers(infos, (err: any, result: any) => {
+                        if (err) {
+                            console.debug('[handleOAuthUsersAction] | error[addUsers] = ', err);
+                            return resolve({ error: true, message: "internal error" });
+                        } else {
+                            console.debug('[handleOAuthUsersAction] | result[addUsers] = ', result);
+                            return resolve({ error: false, message: "user created", result: result });
+                        }
+                    });
+                } else {
+                    return resolve({ error: false, message: "user already exists", result: result });
+                }
             }
+        });
     });
-}
-
-export const registerGoogleUser = (req: express.Request, res: express.Response) => {
-    const { mail, username } = req.body;
-
-    addGoogleUser(username, mail, (err: any, result: any) => {
-        if (err) {
-            console.log(err);
-            return res.status(409).json({ error: true, message: "You might already have registered with this account" });
-        } else {
-            return res.status(200).json({ error: false, message: "user created", result: result });
-        }
-    });
-}
+};
