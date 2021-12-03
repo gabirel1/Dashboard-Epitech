a0659b8e404175a5f4aa53cb90a732e273722911
gabirel <gabriel.knies@epitech.eu>
2021-12-03 17:38:45 +0100

    [main]: fixing door

--- server/src/routes/widgets/epitechDoors.ts
+++ server/src/routes/widgets/epitechDoors.ts
@@ -4,15 +4,15 @@ import axios, { AxiosResponse } from 'axios';
 class EpitechDoors {
     async openDoor(req: express.Request, res: express.Response) {
         try {
-            const { autologin, door_name } = req.body;
-            let autoLogin: string = autologin;
+            const { intra_autologin, door_name } = req.body;
+            let autoLogin: string = intra_autologin;
             let url: string = "";
 
             if (autoLogin.startsWith("https://intra.epitech.eu/") == false) {
-                autoLogin = `https://intra.epitech.eu/${autologin}`;
+                autoLogin = `https://intra.epitech.eu/${intra_autologin}`;
             }
             console.log('autologin == ', autoLogin);
-            url = `https://epi-logue.eu/api/doors_open?login=${autologin}&door_name=${door_name}`;
+            url = `https://epi-logue.eu/api/doors_open?login=${autoLogin}&door_name=${door_name}`;
             let response: AxiosResponse = await axios({
                 method: 'get',
                 url: url,
